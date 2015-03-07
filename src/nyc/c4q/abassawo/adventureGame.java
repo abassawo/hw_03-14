8e154d536d9f1f8d38dd3738e63123f0acf4068f
abassawo <abass.bayo@gmail.com>
2015-03-07 16:17:40 -0500

    Initial Commit

---
+++ src/nyc/c4q/abassawo/adventureGame.java
@@ -0,0 +1,18 @@
+package nyc.c4q.abassawo;
+
+/**
+ * Created by c4q-Abass on 3/7/15.
+ */
+import java.util.Scanner;
+public class adventureGame {
+    public static void main (String[] args ) {
+        Scanner gameInput = new Scanner(System.in);
+        int Portal;
+
+        System.out.println("You are trapped in an alternate universe, and you need to solve some clues to escape");
+        System.out.println("Which portal would you like to use?" );
+        Portal = gameInput.nextInt();
+
+
+    }
+}
