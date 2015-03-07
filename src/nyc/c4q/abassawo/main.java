8c49b2e50a14777e168003cf5a8242eece73823e
abassawo <abass.bayo@gmail.com>
2015-03-07 16:49:24 -0500

    added whole src folder

--- src/nyc/c4q/abassawo/main.java
+++ src/nyc/c4q/abassawo/main.java
@@ -5,7 +5,7 @@ package nyc.c4q.abassawo;
  */
 import java.util.Scanner;
 
-public class main {
+public class Main {
     public static void main (String[] args) {
         Scanner gameInput = new Scanner(System.in);
         int Portal;
@@ -14,10 +14,19 @@ public class main {
         System.out.print("\nPortal 1 requires you to " +
                 "solve a logic game to move to the next level." + "\nPortal 2 requires you to solve a mystery" +
                 "\nPortal 3 requires you to solve a riddle " + "\nPortal 4 is unknown " +
-                "\nWhich portal would you like to enter?");
+                "\nWhich portal would you like to enter? ");
         Portal = gameInput.nextInt();
-        System.out.println("You chose " + Portal);
-
-
+        if (Portal == 1) {
+            System.out.println("You chose Portal " + Portal + " You must solve a logic game");
+        } else if (Portal == 2) {
+            System.out.println("You chose Portal " + Portal + " You must solve a mystery...");
+        } else if (Portal == 3) {
+            System.out.println("You chose Portal " + Portal + " You must solve a riddle");
+        } else if (Portal ==4) {
+            System.out.println("You have chosen Portal " + Portal + ": the realm of the unknown...You're doomed");
+        } else {
+            System.out.println("Sorry, that's not an option");
+        }
+        //Game options: Different portals present different opportunities for the player to rack up escape points.
     }
 }
